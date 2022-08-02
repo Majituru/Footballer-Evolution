79b171a49cf655d3de28226c13e0a9469c9a4878
Maji <biltangurleme@gmail.com>
2022-08-02 23:39:28 +0300

    Money - Goal Keeper

---
+++ Footballer-Evolution/Assets/DragMove.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragMove : MonoBehaviour
+{
+
+    private Touch touch;
+    private float speedModifier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedModifier = 0.005f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Moved)
+            {
+                transform.position = new Vector3(
+                    transform.position.x ,
+                    transform.position.y,
+                    transform.position.z + touch.deltaPosition.x * speedModifier
+                    );
+            }
+        }
+    }
+}
